c7ad72a138c8ea723660fce8569bd18cf4f55918
Jahidhasan3323 <30716686+Jahidhasan3323@users.noreply.github.com>
2020-09-14 17:41:13 +0600

    search url

---
+++ storage/framework/views/e6bacc1c0200578cec918ab6da2b0c268385901d.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('mainContent'); ?>
+
+	<router-view></router-view>
+							
+<?php $__env->stopSection(); ?>
+
+
+
+
+
+<?php echo $__env->make('main_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp7.4\htdocs\mediusware_bulk1\resources\views/home.blade.php ENDPATH**/ ?>
